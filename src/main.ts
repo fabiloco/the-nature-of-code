73e9fa9274fc27d2a64aab01f101c0ef7e2664d5
Fabián Sánchez <faalsaru@gmail.com>
2023-09-06 09:50:35 -0500

    storing actual excersise in localstorage

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import './style.css';
 
 // document.getElementById('ex_1_1')!.addEventListener('click', () => {});
 
+window.addEventListener('load', () => {
+  let activeExcersie = localStorage.getItem('active-excersise');
+
+  if (activeExcersie) {
+    import(`./${activeExcersie}?version=${Number(new Date())}`);
+  }
+});
+
 document
   .querySelectorAll<HTMLLIElement>('#excersise')
   .forEach((element: HTMLLIElement) => {
@@ -9,5 +17,9 @@ document
       document.getElementById('excersise-canvas')!.innerHTML = '';
       import(`./${element.dataset.src}?version=${Number(new Date())}`);
       document.getElementById('title')?.scrollIntoView();
+
+      if (element.dataset.src) {
+        localStorage.setItem('active-excersise', element.dataset.src);
+      }
     });
   });
